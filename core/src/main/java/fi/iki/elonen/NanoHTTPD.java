8d6a97830abf9685bc0724c24766c8f9704aacd2
Christopher Lane <lanechr@google.com>
2014-06-20 13:54:30 -0700

    Added TEMPORARY_REDIRECT status
    Change-Id: Iaddc6db6a82749a431d7d177bfd569dc320a6b32

--- core/src/main/java/fi/iki/elonen/NanoHTTPD.java
+++ core/src/main/java/fi/iki/elonen/NanoHTTPD.java
@@ -743,10 +743,27 @@ public abstract class NanoHTTPD {
          * Some HTTP response status codes
          */
         public enum Status implements IStatus {
-            SWITCH_PROTOCOL(101, "Switching Protocols"), OK(200, "OK"), CREATED(201, "Created"), ACCEPTED(202, "Accepted"), NO_CONTENT(204, "No Content"), PARTIAL_CONTENT(206, "Partial Content"), REDIRECT(301,
-                "Moved Permanently"), NOT_MODIFIED(304, "Not Modified"), BAD_REQUEST(400, "Bad Request"), UNAUTHORIZED(401,
-                "Unauthorized"), FORBIDDEN(403, "Forbidden"), NOT_FOUND(404, "Not Found"), METHOD_NOT_ALLOWED(405, "Method Not Allowed"), RANGE_NOT_SATISFIABLE(416,
-                "Requested Range Not Satisfiable"), INTERNAL_ERROR(500, "Internal Server Error");
+            SWITCH_PROTOCOL(101, "Switching Protocols"),
+
+            OK(200, "OK"),
+            CREATED(201, "Created"),
+            ACCEPTED(202, "Accepted"),
+            NO_CONTENT(204, "No Content"),
+            PARTIAL_CONTENT(206, "Partial Content"),
+
+            REDIRECT(301, "Moved Permanently"),
+            TEMPORARY_REDIRECT(302, "Moved Temporarily"),
+            NOT_MODIFIED(304, "Not Modified"),
+
+            BAD_REQUEST(400, "Bad Request"),
+            UNAUTHORIZED(401, "Unauthorized"),
+            FORBIDDEN(403, "Forbidden"),
+            NOT_FOUND(404, "Not Found"),
+            METHOD_NOT_ALLOWED(405, "Method Not Allowed"),
+            RANGE_NOT_SATISFIABLE(416, "Requested Range Not Satisfiable"),
+
+            INTERNAL_ERROR(500, "Internal Server Error");
+
             private final int requestStatus;
             private final String description;
 
