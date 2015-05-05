22c710cc07452eab3c189d471cc63f6dd1eaa4fa
Andrei Kapishnikov <kapishnikov@google.com>
2015-05-05 15:54:54 -0400

    Added LOCAL_SDK_VERSION to Android.mk make file
    Without the version set, the component cannot be built.
    
    Change-Id: I088c5654de5e8384dd41fa31d3868ad45856c632

--- Android.mk
+++ Android.mk
@@ -8,5 +8,6 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-java-files-under, core/src/main)
 LOCAL_MODULE := libnanohttpd
 LOCAL_MODULE_TAGS := optional
+LOCAL_SDK_VERSION := current
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
